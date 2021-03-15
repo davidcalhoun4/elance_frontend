9cec59b30576630313bc3bfa2be4f0c15067f57d
david <calhoun5321@gmail.com>
2021-03-15 10:51:19 -0700

    adds genre search by to dancer index

--- src/views/dancers/DancersIndex.vue
+++ src/views/dancers/DancersIndex.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="dancers-index">
     <h1>Dancers</h1>
-    <div v-for="dancer in dancers" v-bind:key="dancer.id">
+    <label for="genre-id">Search By Genre:</label>
+    <select name="genre" v-model="filter">
+      <option>Ballet</option>
+      <option>Modern</option>
+      <option>Contemporary</option>
+      <option>Tap</option>
+      <option>Hip-Hop</option>
+    </select>
+    <div
+      v-for="dancer in filterBy(dancers, filter, 'genre')"
+      v-bind:key="dancer.id"
+    >
       <router-link :to="`/dancers/${dancer.id}`">
         <h2>{{ dancer.first_name }} {{ dancer.last_name }}</h2>
         <h3>{{ dancer.email }}</h3>
@@ -14,10 +25,13 @@
 
 <script>
 import axios from "axios";
+import Vue2Filters from "vue2-filters";
 export default {
+  mixins: [Vue2Filters.mixin],
   data: function() {
     return {
       dancers: [],
+      filter: "",
     };
   },
   created: function() {
